c5e312c6a76870776a7ffdd1aa428b29efa3f99c
KEI$HO <133760530+N-Keisho@users.noreply.github.com>
2024-11-21 23:47:24 +0900

    Add Low-Poly Simple Nature Pack

---
+++ Assets/BattleManager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BattleManager : MonoBehaviour
+{
+    public GameObject playerObj;
+    public GameObject enemyObj;
+    private Monster player;
+    private Monster enemy;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
